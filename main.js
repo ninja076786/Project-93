6997eddf78b335e67a61559114f639981afe3b2d
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-05-29 16:33:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function goto(){
+    inputi_____name=document.getElementById("inputi_name").value;
+   localStorage.setItem("inputi_____name",inputi_____name);
+   window.location="kwitter_room.html";
+}
