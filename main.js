4d496d58f2451e26b9efaf3590b1cda00bc6ca19
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-06-10 08:20:35 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function goto(){
-    user_name=document.getElementById("inputi_name").value;
+    user_name=document.getElementById("input_name").value;
    localStorage.setItem("user_name",user_name);
    window.location="kwitter_room.html";
 }
