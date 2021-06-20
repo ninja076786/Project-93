8c3c529ba3f7cae64af83b1bb1cbe0756710ce70
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-06-19 17:07:58 -0700

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,4 @@
-jongla=document.getElementById("input_name").value;
-   document.getElementById("name_here").innerHTML="Welcome "+jongla+" !"
+
 var firebaseConfig = {
     apiKey: "AIzaSyAYQmNV_v5zLPCB7MDvK6h-w_NvMv5RXWg",
     authDomain: "project-93-kwitter-revamp.firebaseapp.com",
@@ -10,42 +9,42 @@ var firebaseConfig = {
     appId: "1:731956554305:web:ad1879207c8574b057b4c0"
   };
   firebase.initializeApp(firebaseConfig);
-function logout(){
-    window.location="index.html";
-}
+  jongla=document.getElementById("room").value;
+   document.getElementById("name_here").innerHTML="Welcome "+jongla+" !";
+
+   function getData() {firebase.database().ref("/").on('value',
+   function(snapshot) {document.getElementById("output").innerHTML =
+   "";snapshot.forEach(function(childSnapshot) {childKey =
+   childSnapshot.key;
+    Room_names = childKey;
+    //Start code
+    var row="<div class='room_name' id='+Room_names+' onclick='redirect(this.id)' style='cursor:pointer;'># "+Room_names+"</div><hr>";
+    document.getElementById("output").innerHTML=row
+    //End code
+    });});}
+
+    getData();
+    
 function addRoom(){
   var mocha1=document.getElementById("room_name").value;
   firebase.database().ref("/").child(mocha1).update({
     ALERT_ALERT_ALERT_ALERT_ALERT1:"A Room Has Been Added!!!"
     });
-    localStorage.setItem("mocha1",mocha1);
-    function getData() {firebase.database().ref("/").on('value',
-function(snapshot) {document.getElementById("output").innerHTML =
-"";snapshot.forEach(function(childSnapshot) {childKey =
-childSnapshot.key;
- Room_names = childKey;
- //Start code
-
- //End code
- });});}
-getData();
+  }
+  localStorage.setItem("mocha1",mocha1);
 window.location("kwitter_page.html");
-document.getElementById("output").innerHTML=row
-}
+
 function gotoroom(){
   var mocha2=document.getElementById("room_name").value;
   firebase.database().ref("/").child(mocha2).update({
     ALERT_ALERT_ALERT_ALERT_ALERT2:"null Will Go To A Trending Room!!!"
     });
+    window.location("kwitter_page.html");
+  }
     localStorage.setItem("mocha2",mocha2);
-    function getData() {firebase.database().ref("/").on('value',
-function(snapshot) {document.getElementById("output").innerHTML =
-"";snapshot.forEach(function(childSnapshot) {childKey =
-childSnapshot.key;
- Room_names = childKey;
- var row="<div class='room_name' id='+Room_names+' onclick='redirect(this.id)' style='cursor:pointer;'># "+Room_names+"</div><hr>";
- });});}
-getData();
-window.location("kwitter_page.html");
-document.getElementById("output").innerHTML=row
+
+
+
+function logout(){
+  window.location="index.html";
 }
